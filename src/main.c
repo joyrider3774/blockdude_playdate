ecab97b826b6e08da72883f8a55d51afe3a103dc
Willems Davy <joyrider3774@hotmail.com>
2023-01-27 20:43:39 +0100

    add playdate.store id

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@ int eventHandler(PlaydateAPI* playdate, PDSystemEvent event, uint32_t arg)
 	if ( event == kEventInit )
 	{
 		setPDPtr(playdate);
+		playdate->system->logToConsole("app.uuid=664315b417744969aa93577f23e9879e");
 		playdate->display->setRefreshRate(FRAMERATE);
 		playdate->system->setUpdateCallback(mainLoop, NULL);
 		setupGame();
